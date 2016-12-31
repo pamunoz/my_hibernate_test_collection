bbadb917b63ba318efef0dfb2197ca3d1d9c1d13
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-30 22:19:39 -0300

    Add icon

--- sceneBuilderTest/src/main/java/com/pfariasmunoz/scenebuildertest/MainApp.java
+++ sceneBuilderTest/src/main/java/com/pfariasmunoz/scenebuildertest/MainApp.java
@@ -10,6 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -53,6 +54,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        
+        // Add the application icon
+        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
         initRootLayout();
 
