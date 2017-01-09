db2d813e9a64947683d7488ffd77b747b72a7bca
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-09 19:24:57 -0300

    Add Configuration Collection and primary keys

--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/UserDetails.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/UserDetails.java
@@ -1,13 +1,21 @@
 package com.pfariasmunoz.firsthibernateproj.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table (name = "USER_DETAILS")
@@ -20,7 +28,14 @@ public class UserDetails {
     private String userName;
     
     @ElementCollection
-    private Set<Address> listOfAddresses = new HashSet();
+    @JoinTable (name = "USER_ADRESS",
+            joinColumns = @JoinColumn(name = "USER_ID"))
+    @GenericGenerator(name="sequence-gen",strategy="sequence")
+    @CollectionId (columns = {
+            @Column(name = "ADDRESS_ID")}, 
+            generator = "sequence-gen", 
+            type = @Type(type = "long"))
+    private Collection<Address> listOfAddresses = new ArrayList<>();
 
     public int getUserId() {
         return userId;
@@ -36,15 +51,14 @@ public class UserDetails {
 
     public void setUserName(String userName) {
         this.userName = userName;
-    } 
+    }
 
-    public Set<Address> getListOfAddresses() {
+    public Collection<Address> getListOfAddresses() {
         return listOfAddresses;
     }
 
-    public void setListOfAddresses(Set<Address> listOfAddresses) {
+    public void setListOfAddresses(Collection<Address> listOfAddresses) {
         this.listOfAddresses = listOfAddresses;
     }
-    
     
 }
