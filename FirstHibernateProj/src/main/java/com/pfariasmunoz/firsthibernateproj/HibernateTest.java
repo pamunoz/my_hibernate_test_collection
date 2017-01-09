6f430184cc6126c88791a6d21a7efddf6744a73e
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-09 12:18:48 -0300

    Add session get to get an Object from the Db

--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
@@ -15,19 +15,27 @@ public class HibernateTest {
                 .configure() // configures settings from hibernate.cfg.xml
                 .buildSessionFactory();
  
-        try (Session session = sessionFactory.openSession()) {
-            Transaction tx = session.beginTransaction();
-            UserDetails user = new UserDetails();
-            user.setUserId(1);
-            user.setUserName("First User");
-            user.setAddress("First user adress");
-            user.setJoinedDate(new Date());
-            user.setDescription("description of the user goes here");
-            
-            
-            session.save(user);
-            tx.commit();
-        }
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        UserDetails user = new UserDetails();
+        user.setUserId(1);
+        user.setUserName("Pablo");
+        user.setAddress("First user adress");
+        user.setJoinedDate(new Date());
+        user.setDescription("description of the user goes here");
+
+        session.save(user);
+        tx.commit();
+        session.close();
+        user = null;
+        
+        session = sessionFactory.openSession();
+        Transaction tx2 = session.beginTransaction(); 
+        
+        user = (UserDetails) session.get(UserDetails.class, 1);
+        System.out.println("User Name retrieved is " + user.getUserName());
+       
         
     }
     
