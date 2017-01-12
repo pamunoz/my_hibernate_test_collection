a266abe8ba2a82e8c3dc65671a1f4c328084f4eb
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-12 14:43:57 -0300

    Add hibernate examples

---
+++ examples_hibernate/session_instance.java
@@ -0,0 +1,14 @@
+Session session = factory.openSession();
+Transaction tx = null;
+try {
+   tx = session.beginTransaction();
+   // do some work
+   ...
+   tx.commit();
+}
+catch (Exception e) {
+   if (tx!=null) tx.rollback();
+   e.printStackTrace(); 
+}finally {
+   session.close();
+}
