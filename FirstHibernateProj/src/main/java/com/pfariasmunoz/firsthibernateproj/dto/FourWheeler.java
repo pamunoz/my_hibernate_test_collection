b1baf63d84f3a32fd1aa77be64f9d66564466d72
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-14 12:20:10 -0300

    Add classes for inheritance

---
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/FourWheeler.java
@@ -0,0 +1,14 @@
+package com.pfariasmunoz.firsthibernateproj.dto;
+
+public class FourWheeler extends Vehicle {
+    private String mSteeringWheel;
+
+    public String getSteeringWheel() {
+        return mSteeringWheel;
+    }
+
+    public void setSteeringWheel(String SteeringWheel) {
+        this.mSteeringWheel = SteeringWheel;
+    }
+    
+}
---
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/TwoWheeler.java
@@ -0,0 +1,14 @@
+package com.pfariasmunoz.firsthibernateproj.dto;
+
+public class TwoWheeler extends Vehicle {
+    
+    private String mSteeringHandle;
+
+    public String getSteeringHandle() {
+        return mSteeringHandle;
+    }
+
+    public void setSteeringHandle(String SteeringHandle) {
+        this.mSteeringHandle = SteeringHandle;
+    }
+}
