cc477b095974b725d391c0ce6a7f13c6487ff326
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-15 15:15:58 -0300

    Add simple query in HQL

--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
@@ -1,12 +1,10 @@
 package com.pfariasmunoz.firsthibernateproj;
 
-import com.pfariasmunoz.firsthibernateproj.dto.Address;
 import com.pfariasmunoz.firsthibernateproj.dto.FourWheeler;
-import com.pfariasmunoz.firsthibernateproj.dto.Shoe;
 import com.pfariasmunoz.firsthibernateproj.dto.TwoWheeler;
-import com.pfariasmunoz.firsthibernateproj.dto.UserDetails;
 import com.pfariasmunoz.firsthibernateproj.dto.Vehicle;
-import java.util.Date;
+import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -15,43 +13,6 @@ import org.hibernate.cfg.Configuration;
 public class HibernateTest {
     
     public static void main(String[] args) {
-        
-//        // Creating Users
-//        UserDetails user = new UserDetails();       
-//        user.setUserName("Pablo");
-//        
-//        UserDetails cris = new UserDetails();
-//        cris.setUserName("Cristopher");
-        
-        // Creating Vehicles
-        Vehicle vehicle = new Vehicle();
-        vehicle.setVehicleName("Tesla Model 3");
-        
-        Vehicle vehicle2 = new Vehicle();
-        vehicle2.setVehicleName("Toyota");
-        
-        // Createing sub classes of vehicle
-        FourWheeler car = new FourWheeler();
-        car.setVehicleName("Porsche");
-        car.setmSteeringWheel("Porsche Steering Wheel");
-        
-        
-        TwoWheeler bike = new TwoWheeler();
-        bike.setVehicleName("Bike");
-        bike.setmSteeringHandle("Bike Steering Handle");
-        
-        
-//        //Crating Shoes
-//        Shoe zapatilla = new Shoe();
-//        zapatilla.setBrand("Nike");
-//        Shoe zapato = new Shoe();
-//        zapato.setBrand("Hush Puppies");
-//        
-//        user.getCalzado().add(zapatilla);
-//        user.getCalzado().add(zapato);
-//        zapato.setOwner(cris);
-//        zapatilla.setOwner(user);       
-        
               
         SessionFactory sessionFactory;
         sessionFactory = new Configuration()
@@ -60,17 +21,10 @@ public class HibernateTest {
  
         try (Session session = sessionFactory.openSession()) {
             Transaction tx = session.beginTransaction();
-//             save the shoes
-//            session.save(zapatilla);
-//            session.save(zapato);
-            // save the vehicles
-            session.save(vehicle);
-            session.save(vehicle2);
-            session.save(bike);
-            session.save(car);
-//             save the users
-//            session.persist(cris);
-//            session.persist(user);
+            
+            Query query = session.createQuery("from UserDetails");
+            List users = query.list();
+            System.out.println("Size of list result = " + users.size());
 
             tx.commit();
         }       
--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/UserDetails.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/UserDetails.java
@@ -3,18 +3,11 @@ package com.pfariasmunoz.firsthibernateproj.dto;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.persistence.CascadeType;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import org.hibernate.annotations.SelectBeforeUpdate;
 
 @Entity
@@ -25,11 +18,7 @@ public class UserDetails {
     @GeneratedValue (strategy = GenerationType.AUTO)
     private int userId;
        
-    private String userName;
-    
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.PERSIST)
-    private Collection<Shoe> calzado = new ArrayList<Shoe>();
-    
+    private String userName;    
     
     public int getUserId() {
         return userId;
@@ -46,15 +35,4 @@ public class UserDetails {
     public void setUserName(String userName) {
         this.userName = userName;
     }
-
-    public Collection<Shoe> getCalzado() {
-        return calzado;
-    }
-
-    public void setCalzado(Collection<Shoe> calzado) {
-        this.calzado = calzado;
-    }
-    
-    
-    
 }
