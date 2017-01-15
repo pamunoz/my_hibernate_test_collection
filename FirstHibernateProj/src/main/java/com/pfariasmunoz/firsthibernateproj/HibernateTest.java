910cb947a5b918c9f36471633f7f1a510ee085e7
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-15 20:26:41 -0300

    Add simple query for only fields

--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
@@ -20,17 +20,18 @@ public class HibernateTest {
         try (Session session = sessionFactory.openSession()) {
             Transaction tx = session.beginTransaction();
             
-            Query query = session.createQuery("from UserDetails where userId > 5");
+            Query query = session.createQuery("select userName from UserDetails");
             // Adding pagination
             query.setFirstResult(2);
             query.setMaxResults(3);
             
             
-            List<UserDetails> users = (List<UserDetails>)query.list();
-            System.out.println("Size of list result = " + users.size());
+//            List<UserDetails> users = (List<UserDetails>)query.list();
+            List<String> userNames = (List<String>) query.list();
+            System.out.println("Size of list result = " + userNames.size());
             
-            users.forEach((user) -> {
-                System.out.println("User name: " + user.getUserName());
+            userNames.forEach((userName) -> {
+                System.out.println("User name: " + userName);
             });
 
             tx.commit();
