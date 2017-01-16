95f2ce24c8a2ba746f8484cfd45ab566047509e2
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-15 22:33:42 -0300

    Add an equal restriction

--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/HibernateTest.java
@@ -1,58 +1,18 @@
 package com.pfariasmunoz.firsthibernateproj;
 
-import com.pfariasmunoz.firsthibernateproj.dto.Address;
-import com.pfariasmunoz.firsthibernateproj.dto.FourWheeler;
-import com.pfariasmunoz.firsthibernateproj.dto.Shoe;
-import com.pfariasmunoz.firsthibernateproj.dto.TwoWheeler;
 import com.pfariasmunoz.firsthibernateproj.dto.UserDetails;
-import com.pfariasmunoz.firsthibernateproj.dto.Vehicle;
-import java.util.Date;
+import java.util.List;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 
 public class HibernateTest {
     
     public static void main(String[] args) {
         
-//        // Creating Users
-//        UserDetails user = new UserDetails();       
-//        user.setUserName("Pablo");
-//        
-//        UserDetails cris = new UserDetails();
-//        cris.setUserName("Cristopher");
-        
-        // Creating Vehicles
-        Vehicle vehicle = new Vehicle();
-        vehicle.setVehicleName("Tesla Model 3");
-        
-        Vehicle vehicle2 = new Vehicle();
-        vehicle2.setVehicleName("Toyota");
-        
-        // Createing sub classes of vehicle
-        FourWheeler car = new FourWheeler();
-        car.setVehicleName("Porsche");
-        car.setmSteeringWheel("Porsche Steering Wheel");
-        
-        
-        TwoWheeler bike = new TwoWheeler();
-        bike.setVehicleName("Bike");
-        bike.setmSteeringHandle("Bike Steering Handle");
-        
-        
-//        //Crating Shoes
-//        Shoe zapatilla = new Shoe();
-//        zapatilla.setBrand("Nike");
-//        Shoe zapato = new Shoe();
-//        zapato.setBrand("Hush Puppies");
-//        
-//        user.getCalzado().add(zapatilla);
-//        user.getCalzado().add(zapato);
-//        zapato.setOwner(cris);
-//        zapatilla.setOwner(user);       
-        
-              
         SessionFactory sessionFactory;
         sessionFactory = new Configuration()
                 .configure() // configures settings from hibernate.cfg.xml
@@ -60,20 +20,17 @@ public class HibernateTest {
  
         try (Session session = sessionFactory.openSession()) {
             Transaction tx = session.beginTransaction();
-//             save the shoes
-//            session.save(zapatilla);
-//            session.save(zapato);
-            // save the vehicles
-            session.save(vehicle);
-            session.save(vehicle2);
-            session.save(bike);
-            session.save(car);
-//             save the users
-//            session.persist(cris);
-//            session.persist(user);
-
+            
+            Criteria criteria = session.createCriteria(UserDetails.class);
+            criteria.add(Restrictions.eq("userName", "User 10"));
+            
+            List<UserDetails> users = (List<UserDetails>) criteria.list();
             tx.commit();
-        }       
+            
+            for (UserDetails user : users) {
+                System.out.println("User Name: " + user.getUserName());
+            }
+        }  
     }
     
 }
--- FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/UserDetails.java
+++ FirstHibernateProj/src/main/java/com/pfariasmunoz/firsthibernateproj/dto/UserDetails.java
@@ -3,18 +3,11 @@ package com.pfariasmunoz.firsthibernateproj.dto;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.persistence.CascadeType;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import org.hibernate.annotations.SelectBeforeUpdate;
 
 @Entity
